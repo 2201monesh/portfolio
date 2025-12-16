0bc42b934f392c227ebc72fe383c2bf594ddf7a9
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-12-17 00:59:22 +0530

    Middle section

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,22 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black flex-col">
       <div className="w-2/5 h-screen">
         <div className="flex items-center mt-18">
-          <div className="w-13 h-13 flex items-center justify-center rounded-full bg-neutral-300 mr-4"></div>
+          <div className="w-14 h-14 flex items-center justify-center rounded-full bg-neutral-300 mr-4"></div>
           <div className="flex flex-col">
             <p className="">Monesh Goyal</p>
             <p className="text-neutral-600">Software Engineer</p>
           </div>
         </div>
-        <div className="mt-20">
-          <p className="text-neutral-600">
+        <div className="mt-18">
+          <p className="text-neutral-600 mb-4">
             Building software with a focus on design, detail, and function.
           </p>
+          <p className="text-neutral-600 mb-4">
+            I'm building a design-engineering studio working with AI <br />{" "}
+            companies on interfaces, systems, and marketing sites. <br /> Recent
+            collaborations include: Cloudflare, LMArena, and Antimetal.
+          </p>
+          <p className="text-neutral-600">My Work:</p>
         </div>
       </div>
     </div>
