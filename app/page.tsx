eb5765a85612ffab3a4b8fee93049fe5d4156138
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-04-16 15:33:13 +0530

    added folder card animation component in work section

--- app/page.tsx
+++ app/page.tsx
@@ -98,13 +98,16 @@ export default function Home() {
     <p className="text-sm leading-snug">Engineering and design</p>
   </header>
   <div className="space-y-4">
-    <p className="leading-7">I'm a frontend engineer who enjoys building clean, modern UIs with smooth, minimal animations. I mostly work with React, Next.js, Tailwind CSS, and shadcn/ui, and I love turning well-thought-out designs into fast, polished web experiences.</p>
-    <p className="leading-7">These days, I spend a lot of time using Framer Motion to add subtle motion-micro-interactions, transitions, and animations that make interfaces feel natural without being flashy. I care a lot about visual consistency, performance, and writing code that's easy to scale and maintain.</p>
-    <p className="leading-7">Outside work, I experiment with side projects, explore new tools, and constantly level up my craft.</p>
-    <p className="leading-7">For collaborations, reach me at <span className="link-text">moneshgoyal.work@gmail.com</span>. I also share about my work on <a href="https://x.com/moneshgoyal" target="_blank" rel="noopener noreferrer" className="link-text">X (Twitter)</a>. You can also see my project work on <a href="https://github.com/2201monesh" target="_blank" rel="noopener noreferrer" className="link-text">GitHub</a> and see my work experiences on <a href="https://www.linkedin.com/in/monesh-goyal/" target="_blank" rel="noopener noreferrer" className="link-text">LinkedIn</a>.</p>
+    <p className="leading-7">I’m a design engineer who loves crafting clean, modern interfaces that feel right. I spend a lot of time working on UI + motion - making components that are smooth, intentional, and just naturally satisfying to use.</p>
+    <p className="leading-7">I work with React, Next.js, Tailwind, and shadcn/ui, and lately I’ve been diving deep into framer motion - crafting micro interactions and transitions that truly elevate the experience. Big on details like timing, consistency, and performance, and I take pride in building some of the cleanest motion components out there.</p>
+    <p className="leading-7">General mails at <span className="link-text">moneshgoyal.work@gmail.com</span>. I also share about my work on <a href="https://x.com/moneshgoyal" target="_blank" rel="noopener noreferrer" className="link-text">X (Twitter)</a>. You can also see my project work on <a href="https://github.com/2201monesh" target="_blank" rel="noopener noreferrer" className="link-text">GitHub</a> and see my work experiences on <a href="https://www.linkedin.com/in/monesh-goyal/" target="_blank" rel="noopener noreferrer" className="link-text">LinkedIn</a>.</p>
   </div>
   <div className="mt-12 space-y-4 select-none">
     <div className="divide-y divide-neutral-100 dark:divide-neutral-800">
+       <a href="https://x.com/moneshgoyal/status/2043915778916004018?s=20" target="_blank" rel="noopener noreferrer" className="flex cursor-pointer items-center justify-start py-2 text-sm hover:bg-neutral-50 dark:hover:bg-neutral-800">
+        <p className="w-30 text-neutral-400">Apr 14, 2026</p>
+        <p className="">Folder card animation</p>
+      </a>
       <a href="https://x.com/moneshgoyal/status/2040771299677778423?s=20" target="_blank" rel="noopener noreferrer" className="flex cursor-pointer items-center justify-start py-2 text-sm hover:bg-neutral-50 dark:hover:bg-neutral-800">
         <p className="w-30 text-neutral-400">Apr 5, 2026</p>
         <p className="">Motion cards component</p>
